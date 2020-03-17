145025fc9ba3c88d35180adf281a4821963f789e
Galgóczi Ferenc <galgoczi.ferenc@gmail.com>
2020-03-17 13:30:48 +0100

    Initial commit.

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.TimeUnit;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		sayHello();
+		for (int i = 50; i < 2000; i *= 3) {
+			System.out.println(i);
+		}
+		int i = isGood(30) ? 75 : isGood(25) ? 30 : 100;
+		System.out.println(i);
+		sayBye();
+	}
+
+	private static boolean isGood(int i) {
+		return i % 15 == 0 && i * 2 < 75;
+	}
+
+	static void sayHello() {
+		String x = "hello";
+	}
+
+	static void sayBye() {
+		System.out.println("bye-bye");
+	}
+}
